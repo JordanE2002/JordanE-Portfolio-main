bb18da6b899a2a7481efd165035fb177cb04a15a
Jordan English <JEnglish717@gmail.com>
2025-03-05 18:34:55 +0000

    fixes

--- scheme.php
+++ scheme.php
@@ -20,7 +20,7 @@
         <a href="index.php#projects" class="sidebar-text"> My Projects</a>
         <a href="index.php#contact" class="sidebar-text">Contact Me</a>
         <div>
-            <a href="about-me.phpl#about" class="sidebar-text">About Me</a>
+            <a href="about-me.php#about" class="sidebar-text">About Me</a>
         </div>
         <div>
             <a href="coding-examples.php#coding-examples" class="sidebar-text">Coding Examples</a>
@@ -40,20 +40,7 @@ include("inc/header.php");
 
     
 
-    <!-- Page Content -->
-    <div id="main-content">
-        <!-- Header -->
-        <section id="home" class="header">
-            <div class="header-content">
-                <h1 id="greeting-text"></h1> <!-- This will display "Hello, I am Jordan English." -->
-                <p id="portfolio-text"></p> <!-- This will display "This is my Portfolio." -->
   
-            </div>
-
-            
-            <img src="images/sausage-dog.webp" alt="sausage-dog" class="banner-img">
-        </section>
-
 
 
     <sction id="scheme">
