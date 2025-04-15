1c9b26aaa34cf88f412a71c039eb7a6d1781e2c8
Jordan English <JEnglish717@gmail.com>
2025-04-15 12:55:55 +0100

    code example improvemens

--- coding-examples.php
+++ coding-examples.php
@@ -45,17 +45,33 @@ include("inc/header.php");
 
 
     <!-- Code snippets section -->
-    <section>
-        <h1 id="coding-examples" class="title-centered">Code Snippets</h1>
+    <section id="coding-examples">
+    <h1 class="title-centered">Code Snippets</h1>
 
-        <div class="HTML"><h3>HTML: Nav bar for portfolio, whih takes the user to other pages and i used it as it was easy to make/use and fun to play woth to naviage to other pages</h3>
-        <img src="images/HTML-code-example.png" alt="Project 1">
+    <div class="code-snippet">
+        <h3>HTML: Nav bar for portfolio</h3>
+        <p>This nav bar allows users to navigate to other pages in the portfolio. I used it because it was easy to make and fun to play with. It is also very useful for going to the other pages without any big issues </p>
+        <img src="images/HTML-code-example.png" alt="HTML code example">
     </div>
-        
-        <div class="CSS"><h3>CSS: TBC</h3></div>
-        <div class="JAVA"><h3>JAVA: TBC</h3></div>
-        <div class="PHP"><h3>PHP: TBC</h3></div>
-    </section>
+
+    <div class="code-snippet">
+        <h3>CSS: Styling the hero image</h3>
+        <p>This CSS code is used to style the layout of the top image on the portfolio page, it sets the image, making it fit by the nav without overflowing so i can have this hero image fit nicely next to the nav bar </p>
+        <img src="images/css-code-snippet.png" alt="CSS code example">
+    </div>
+
+    <div class="code-snippet">
+        <h3>JavaScript: Image array change image logic</h3>
+        <p>This JavaScript code shows that when i press a button for the js-array it shall generate a new image using a API to not have to use saved images which would cost time and memory</p>
+        <img src="images/js-array-snippet.png" alt="JavaScript array example">
+    </div>
+
+    <div class="code-snippet">
+        <h3>PHP: Contact form handling</h3>
+        <p>TThis contact section displays a form where users can enter their name, email, subject, and message to get in touch. When the user submits the form, it runs a JavaScript validation function to check the inputs, it goes to process_contact.php where it stores my mailtrap details for email storing. </p>
+        <img src="images/php-code-snippet.png" alt="PHP code example">
+    </div>
+</section>
 </div>
 
 <?php 
